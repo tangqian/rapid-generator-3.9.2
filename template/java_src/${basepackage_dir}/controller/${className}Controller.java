8131395a9d1832e127532d5ad59f7075aaa4032b
tangqian <275855127@qq.com>
2015-07-18 17:45:50 +0800

    修改模板
    应用新的项目，故需要修改模板

--- template/java_src/${basepackage_dir}/controller/${className}Controller.java
+++ template/java_src/${basepackage_dir}/controller/${className}Controller.java
@@ -1,87 +1,119 @@
-/*
- * 文 件 名:  ${table.className}Controller.java
- * 创 建 人:  tangqian
- * 创建时间:  <#if now??>${now?string('yyyy-MM-dd')}</#if>
- */
+<#include "/java_copyright.include">
 <#assign className = table.className>
 <#assign classNameLower = className?uncap_first>
 <#assign shortName = table.shortName>
 package ${basepackage}.controller;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.app.base.common.dto.BaseSearchDto;
-import com.app.base.common.dto.DwzJsonResponse;
-import com.app.base.common.dto.ServiceResult;
-import com.app.base.common.dto.DwzJsonResponse.CallbackTypeEnum;
-import com.app.base.common.web.BaseController;
-import ${basepackage}.dao.${className};
+import com.tq.management.base.controller.BaseController;
+import ${basepackage}.entity.${className};
 import ${basepackage}.service.${className}Service;
+import com.tq.management.base.utils.WebDto;
 
+/**
+ * @version 1.0
+ * @author tangqian
+ */
 @Controller
-@RequestMapping("/${shortName}")
+@RequestMapping("/${classNameLower}")
 public class ${className}Controller extends BaseController {
     
-    private static final String LIST = "${shortName}/${shortName}_list";
+    private static final String LIST = "system/${classNameLower}/${classNameLower}_list";
     
-    private static final String ADD = "${shortName}/${shortName}_add";
+    private static final String ADD = "system/${classNameLower}/${classNameLower}_add";
     
-    private static final String EDIT = "${shortName}/${shortName}_edit";
+    private static final String EDIT = "system/${classNameLower}/${classNameLower}_edit";
     
-    private static final String VIEW = "${shortName}/${shortName}_view";
+    private static final String VIEW = "system/${classNameLower}/${classNameLower}_view";
     
     @Resource
     private ${className}Service ${classNameLower}Service;
     
-    @RequestMapping("/list")
-    public ModelAndView list(HttpServletRequest request) {
-        BaseSearchDto dto = new BaseSearchDto(request);
-        ModelAndView mav = getModelAndView(dto, LIST);
-        mav.addObject("page", ${classNameLower}Service.getList(dto));
-        return mav;
-    }
+	@RequestMapping
+	public ModelAndView page() {
+		return new ModelAndView(LIST);
+	}
+	
+	@RequestMapping(value = "/list")
+	@ResponseBody
+	public Map<String, Object> list() {
+		WebDto dto = new WebDto(getRequest());
+		return ${classNameLower}Service.list(dto);
+	}
     
-    @RequestMapping("/bfAdd")
-    public ModelAndView bfAdd() {
-        return getModelAndView(ADD);
-    }
-    
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public @ResponseBody DwzJsonResponse add(${className} ${classNameLower}) {
-        ServiceResult result = ${classNameLower}Service.save(${classNameLower});
-        return getDefaultJsonResponse(result);
-    }
-    
-    @RequestMapping("/bfEdit")
-    public ModelAndView bfEdit(Integer id){
-        ModelAndView mav = getModelAndView(EDIT);
-        mav.addObject("${shortName}", ${classNameLower}Service.getById(id));
-        return mav;
-    }
-    
-    @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public @ResponseBody DwzJsonResponse edit(${className} ${classNameLower}) {
-        ServiceResult result = ${classNameLower}Service.update(${classNameLower});
-        return getDefaultJsonResponse(result);
-    }
-    
-    @RequestMapping("/view")
-    public ModelAndView view(Integer id){
-        ModelAndView mav = getModelAndView(VIEW);
-        mav.addObject("${shortName}", ${classNameLower}Service.view(id));
-        return mav;
-    }
-    
-    @RequestMapping("/delete")
-    public @ResponseBody DwzJsonResponse delete(String ids) {
-        ServiceResult result = ${classNameLower}Service.delete(ids);
-        return getDefaultJsonResponse(result).setCallbackType(CallbackTypeEnum.NOTHING);
-    }
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ModelAndView add() {
+		return new ModelAndView(ADD);
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> doAdd() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		WebDto dto = new WebDto(getRequest());
+		${classNameLower}Service.add(dto);
+		map.put("status", 1);
+		return map;
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam(required = true) Integer id) {
+		ModelAndView mv = new ModelAndView(EDIT);
+		${className} ${classNameLower} = ${classNameLower}Service.get(id);
+		mv.addObject("entity", ${classNameLower});
+		return mv;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> doEdit() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		WebDto dto = new WebDto(getRequest());
+		${classNameLower}Service.update(dto);
+		map.put("status", 1);
+		return map;
+	}
+	
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public ModelAndView view(@RequestParam(required = true) Integer id) {
+		ModelAndView mv = new ModelAndView(VIEW);
+		${className} ${classNameLower} = ${classNameLower}Service.get(id);
+		mv.addObject("entity", ${classNameLower});
+		return mv;
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> delete(@RequestParam(required = true) Integer id) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		${classNameLower}Service.delete(id);
+		map.put("status", 1);
+		return map;
+	}
+
+	@RequestMapping(value = "/batchDelete", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> batchDelete(@RequestParam(required = true) String ids) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		boolean result = ${classNameLower}Service.batchDelete(ids);
+		if (result) {
+			map.put("status", 1);
+		} else {
+			map.put("status", 0);
+			map.put("msg", "参数为空或者参数值非法");
+		}
+		return map;
+	}
 }
